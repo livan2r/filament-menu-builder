8683da9ae9de16d3bab89248cde448e43288211a
Livan Rodriguez <livan2r@gmail.com>
2025-01-02 21:04:03 -0500

    menu builder customization

--- src/Filament/Resources/MenuResource/Pages/CreateMenu.php
+++ src/Filament/Resources/MenuResource/Pages/CreateMenu.php
@@ -3,9 +3,26 @@
 namespace Biostate\FilamentMenuBuilder\Filament\Resources\MenuResource\Pages;
 
 use Biostate\FilamentMenuBuilder\Filament\Resources\MenuResource;
-use Filament\Resources\Pages\CreateRecord;
+use Filament\Actions\Action;
+use Filament\Actions;
+use App\Filament\Resources\BaseClasses\CreateRecord;
+use Filament\Resources\Pages\CreateRecord\Concerns\Translatable;
 
 class CreateMenu extends CreateRecord
 {
+    Use Translatable;
+
     protected static string $resource = MenuResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->url(MenuResource::getUrl())
+                ->label(__('admin.return'))
+                ->color('gray')
+                ->icon('heroicon-o-arrow-left'),
+            Actions\LocaleSwitcher::make(),
+        ];
+    }
 }
--- src/Filament/Resources/MenuResource/Pages/EditMenu.php
+++ src/Filament/Resources/MenuResource/Pages/EditMenu.php
@@ -4,20 +4,36 @@ namespace Biostate\FilamentMenuBuilder\Filament\Resources\MenuResource\Pages;
 
 use Biostate\FilamentMenuBuilder\Filament\Resources\MenuResource;
 use Biostate\FilamentMenuBuilder\Models\Menu;
+use Filament\Actions\Action;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\ViewRecord\Concerns\Translatable;
 
 class EditMenu extends EditRecord
 {
+    Use Translatable;
+
     protected static string $resource = MenuResource::class;
 
     protected function getActions(): array
     {
         return [
+            Action::make('back')
+                ->url(MenuResource::getUrl())
+                ->label(__('admin.return'))
+                ->color('gray')
+                ->icon('heroicon-o-arrow-left'),
+            Actions\LocaleSwitcher::make(),
             Actions\Action::make(__('filament-menu-builder::menu-builder.configure_menu'))
                 ->url(fn (Menu $record): string => MenuResource::getUrl('build', ['record' => $record]))
-                ->icon('heroicon-o-bars-3'),
-            Actions\DeleteAction::make(),
+                ->color('primary')
+                ->icon('heroicon-o-globe-alt'),
+            Actions\DeleteAction::make()
+                ->icon('heroicon-o-pencil'),
+            Actions\ForceDeleteAction::make()
+                ->icon('heroicon-o-trash'),
+            Actions\RestoreAction::make()
+                ->icon('heroicon-o-refresh'),
         ];
     }
 }
